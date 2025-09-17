5144dd95f0ba3ded160f4109a0b3a4b3ada24e91
Pratiksha Singh <pratiksaah13@gmail.com>
2025-09-17 17:51:13 +0530

    added CORSOrigin

--- src/main/java/com/example/backend/config/CorsConfig.java
+++ src/main/java/com/example/backend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://todo-app-chi-beige.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
